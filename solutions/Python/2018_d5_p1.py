ef1277633f7095511dde5f9acc7601de57d40ceb
Sam Jones <sam.jones1@argos.co.uk>
2019-02-02 22:05:50 +0000

    Add geeky comment.

--- solutions/Python/2018_d5_p1.py
+++ solutions/Python/2018_d5_p1.py
@@ -1,3 +1,11 @@
+#This problem is very close to solving the word problem in a free group, this algorithm is based on the fact that
+#Pushdown machines (those which accept/define context-free language) can solve the word problem for free groups.
+#This (and a nice algebraic extension of this) is known as the muller-schupp theorem: https://en.wikipedia.org/wiki/Muller%E2%80%93Schupp_theorem
+#Ask me if you want to know more.
+
+#You might see that a stack suffices without doing all the group theory, it's just that the group theory
+#is what made me think of this solution.
+
 def parse_input():
 	with open('2018_d5_input.txt', 'r') as f:
 		return f.read().strip()
